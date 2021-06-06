eba760a10c550a80c6026fb4b68041feaaa0d6e8
Matheuszl <matheus.zzalamena@gmail.com>
2021-06-05 22:15:34 -0300

    Criação do projeto spring

---
+++ app-dev/src/main/java/com/dev/appdev/AppDevApplication.java
@@ -0,0 +1,13 @@
+package com.dev.appdev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppDevApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppDevApplication.class, args);
+	}
+
+}
